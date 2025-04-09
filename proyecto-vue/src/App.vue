2f19ff84431d0b8067b9777587517837c4f9f2df
josue cornejo <jg.cornejourbina@ugto.mx>
2025-04-08 20:33:36 -0600

    Se agrego la barra superior con el cuadro de busqueda

--- proyecto-vue/src/App.vue
+++ proyecto-vue/src/App.vue
@@ -4,49 +4,93 @@
 <template>
     <v-app class="app-background">
       <v-navigation-drawer permanent>
-        <v-list-item>
-          <div id="logo">
-            <img src="./assets/logo.png" alt="Logo" width="50px" height="50px">
-            <h1>KANBAN</h1>
-          </div>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-home</v-icon>
-          <a href="">Dashboard</a>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-file-cabinet</v-icon>
-          <a href="">Inventory</a>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-chart-box-outline</v-icon>
-          <a href="">Reports</a>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-account-circle-outline</v-icon>
-          <a href="">Suppliers</a>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-package-variant-closed</v-icon>
-          <a href="">Orders</a>
-        </v-list-item>
-        <v-list-item link>
-          <v-icon>mdi-clipboard-list-outline</v-icon>
-          <a href="">Manage Store</a>
-        </v-list-item>
-        <v-list>
+        <v-list id="lista1">
+          <v-list-item>
+            <div id="logo">
+              <img src="./assets/logo.png" alt="Logo" width="50px" height="50px">
+              <h1>KANBAN</h1>
+            </div>
+          </v-list-item>
+          <v-list-item link id="comienzoLista">
+            <v-icon style="margin-right: 10px;">mdi-home</v-icon>
+            <a href="">Dashboard</a>
+          </v-list-item>
           <v-list-item link>
-            <v-icon>mdi-cog-outline</v-icon>
+            <v-icon style="margin-right: 10px;">mdi-file-cabinet</v-icon>
+            <a href="">Inventory</a>
+          </v-list-item>
+          <v-list-item link>
+            <v-icon style="margin-right: 10px;">mdi-chart-box-outline</v-icon>
+            <a href="">Reports</a>
+          </v-list-item>
+          <v-list-item link>
+            <v-icon style="margin-right: 10px;">mdi-account-circle-outline</v-icon>
+            <a href="">Suppliers</a>
+          </v-list-item>
+          <v-list-item link>
+            <v-icon style="margin-right: 10px;">mdi-package-variant-closed</v-icon>
+            <a href="">Orders</a>
+          </v-list-item>
+          <v-list-item link>
+            <v-icon style="margin-right: 10px;">mdi-clipboard-list-outline</v-icon>
+            <a href="">Manage Store</a>
+          </v-list-item>
+        </v-list>
+        <v-list id="lista2">
+          <v-list-item link>
+            <v-icon style="margin-right: 10px;">mdi-cog-outline</v-icon>
             <a href="">Settings</a>
           </v-list-item>
           <v-list-item link>
-            <v-icon>mdi-logout</v-icon>
+            <v-icon style="margin-right: 10px;">mdi-logout</v-icon>
             <a href="">Log Out</a>
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
+
       <v-main>
-        <h1>cd</h1>
+        <v-row>
+          <v-col >
+            <v-card>
+              <div id="tarjeta1">
+                <div id="contenedor-busqueda">
+                <v-form>
+                  <v-container>
+                    <v-text-field
+                      id="search"
+                      v-model="search"
+                      label="Search product, supplier, order"
+                      prepend-inner-icon="mdi-magnify"
+                    >
+                    </v-text-field>
+                  </v-container>
+                </v-form>
+              </div>
+              <div id="contenedor-notificacion">
+                <v-icon size="30">
+                  mdi-bell-outline
+                </v-icon>
+                <v-icon size="40" style="margin: 0 20px 0 20px;">
+                  mdi-account-circle
+                </v-icon>
+              </div>
+              </div>              
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card id="tarjeta1">
+              <h3>Overall Inventory</h3>
+              
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            col 2
+          </v-col>
+        </v-row>
       </v-main>
     </v-app>
 </template>
@@ -83,8 +127,35 @@
   color: inherit;
 }
 
-.v-divider {
-  margin: 10px 0;
+#tarjeta1 {
+  display: flex;
+  flex-direction: row;
+  height: 90px;
 }
 
+.app-background {
+  background-color: rgb(241, 241, 241)
+}
+
+#contenedor-busqueda {
+  width: 30%;
+  justify-content: center;
+  align-items: center;
+}
+
+#contenedor-notificacion {
+  width: 70%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px;
+}
+
+#comienzoLista {
+  margin-top: 10%;
+}
+
+#lista2{
+  margin-top: 90%;
+}
 </style>
